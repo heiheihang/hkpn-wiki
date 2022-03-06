5fad5886468ace7b6157c9d6c3152e59b299d07e
heiheihang <alexfungshunhang@gmail.com>
2022-03-06 14:21:30 -0500

    modify index

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,18 +8,14 @@ const app_1 = require("firebase/app");
 const auth_1 = require("firebase/auth");
 const firestore_1 = require("firebase/firestore");
 require("dotenv/config");
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
+const firebaseConfig_1 = __importDefault(require("./config/firebaseConfig"));
+const users_1 = __importDefault(require("./routes/users"));
 const app = (0, express_1.default)();
-const firebaseApp = (0, app_1.initializeApp)(firebaseConfig);
+const firebaseApp = (0, app_1.initializeApp)(firebaseConfig_1.default);
 const auth = (0, auth_1.getAuth)(firebaseApp);
 const db = (0, firestore_1.getFirestore)(firebaseApp);
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 (0, auth_1.onAuthStateChanged)(auth, user => {
     if (user != null) {
         console.log("logged in!");
@@ -31,6 +27,8 @@ const db = (0, firestore_1.getFirestore)(firebaseApp);
 app.get('/', (req, res) => {
     res.send('Well done!');
 });
+app.use('/user', users_1.default);
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 });
+//# sourceMappingURL=index.js.map
--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -3,21 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged }  from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import 'dotenv/config'
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-}
+import  firebaseConfig  from './config/firebaseConfig';
+import userRouter from './routes/users';
 
 const app = express();
 const firebaseApp = initializeApp(firebaseConfig)
 const auth = getAuth(firebaseApp)
 const db = getFirestore(firebaseApp)
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 onAuthStateChanged(auth, user => {
     if(user != null) {
         console.log("logged in!")
@@ -29,7 +23,7 @@ onAuthStateChanged(auth, user => {
 app.get('/', (req, res) => {
     res.send('Well done!');
 })
-
+app.use('/user', userRouter);
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
 })
